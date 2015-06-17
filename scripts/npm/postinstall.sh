b32cbb2b7b6a29e007204ea669e60926f87f856b
Chris Trevino <chris.trevino@atsid.com>
2015-06-17 15:08:01 -0700

    Adding Bower for client-side dependency management. * Adding Bluebird client-side library * Adding lodash client-side library

--- scripts/npm/postinstall.sh
+++ scripts/npm/postinstall.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+./node_modules/.bin/bower install
+
 if [ ! -L node_modules/app ]; then
     ln -s ../server node_modules/app
+fi
+
+if [ ! -L client/web_modules ]; then
+    ln -s ../bower_components client/web_modules
 fi
