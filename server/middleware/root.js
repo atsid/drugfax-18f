03f0a364405c15f7fdbcd8272471b47b3a34e61a
Chris Trevino <chris.trevino@atsid.com>
2015-06-23 10:56:19 -0700

    #14 - adding a link to the auth resource in the root

--- server/middleware/root.js
+++ server/middleware/root.js
@@ -2,7 +2,11 @@
 
 let get = (req, res, next) => {
     let payload = {
-        status: "ok"
+        name: "ATS 18F Pool2 Submission",
+        status: "ok",
+        links: {
+            auth: "/auth"
+        }
     };
     res.json(payload);
     next();
