2050a07d7bf4533d36487db867d955598a232359
Chris Trevino <chris.trevino@atsid.com>
2015-06-25 16:46:47 -0700

    #99 - Adding MyDrugs React component

---
+++ client/components/myDrugs.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let React = require("react/addons");
+
+let MyDrugs = React.createClass({
+    render: function() {
+        return <h1> My Drugs </h1>
+    }
+});
+
+module.exports = MyDrugs;
