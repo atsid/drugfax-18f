d1f7077819c9973f98817c581cb4fb6fdc64f5e7
Chris Trevino <chris.trevino@atsid.com>
2015-06-24 18:49:14 -0700

    #13 - sketching out the services

---
+++ server/routers/profile/index.js
@@ -0,0 +1,20 @@
+"use strict";
+let jefferson = require("express-jefferson");
+let mountie = require("express-mountie");
+let debug = require("../../middleware/debug");
+let path = require("path");
+
+let router = jefferson.router({
+    routes: {
+        "/": {
+            "get": [debug.send("NotImplemented - Get Current Profile")]
+        }
+    }
+});
+
+mountie({
+    parent: router,
+    src: path.join(__dirname, "routers"),
+    prefix: (name) => `/${name}`
+});
+module.exports = router;
