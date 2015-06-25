74a74fdba3994dc4383fa79013c85938809d57e9
Chris Trevino <chris.trevino@atsid.com>
2015-06-24 17:03:52 -0700

    #71 - Updating the client-unit-test task so that it does not invoke gulp-exit

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -4,11 +4,12 @@ let mocha = require("gulp-mocha");
 let istanbul = require("gulp-istanbul");
 let isparta = require("isparta");
 let exit = require("gulp-exit");
+let empty = require("gulp-empty");
 let config = require("../config");
 let src = config.globs.src;
 let out = config.globs.out;
 
-let instrument = (sourceGlob, reportDir, callback) => {
+let instrument = (sourceGlob, reportDir, doExit, callback) => {
     return new Promise((resolve, reject) => {
         gulp.src(sourceGlob)
             .pipe(istanbul({
@@ -22,7 +23,7 @@ let instrument = (sourceGlob, reportDir, callback) => {
                         dir: reportDir,
                         reporters: ["lcov", "text-summary"]
                     }))
-                    .pipe(exit())
+                    .pipe(doExit ? exit() : empty())
                     .on("end", resolve)
                     .on("error", reject);
             });
@@ -33,8 +34,8 @@ let runTestBatch = (testGlob) => {
     return gulp.src(testGlob).pipe(mocha({reporter: process.env.TEST_REPORTER || "spec"}));
 };
 
-let runTests = (testGlob, sourceGlob, reportDir) => () => instrument(sourceGlob, reportDir, () => runTestBatch(testGlob));
+let runTests = (testGlob, sourceGlob, reportDir, doExit=true) => () => instrument(sourceGlob, reportDir, doExit, () => runTestBatch(testGlob));
 
 gulp.task("server-unit-test", runTests(src.SERVER_TESTS, src.SERVER_JS, out.SERVER_COVERAGE_OUTPUT));
-gulp.task("client-unit-test", runTests(src.CLIENT_TESTS, src.CLIENT_JS, out.CLIENT_COVERAGE_OUTPUT));
+gulp.task("client-unit-test", runTests(src.CLIENT_TESTS, src.CLIENT_JS, out.CLIENT_COVERAGE_OUTPUT, false));
 gulp.task("test", runTests(src.ALL_TESTS, src.COVERAGE_JS, out.COMBINED_COVERAGE_OUTPUT));
