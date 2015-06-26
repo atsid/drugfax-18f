aa091f3c56ac958c51528a15df810afb1de514d7
Chris Trevino <chris.trevino@atsid.com>
2015-06-25 19:54:36 -0700

    #104 - Updating the test task so that in tdd-mode, it won't kill the gulp stream.

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,5 +1,6 @@
 "use strict";
 let gulp = require("gulp");
+let gutil = require("gulp-util");
 let mocha = require("gulp-mocha");
 let istanbul = require("gulp-istanbul");
 let isparta = require("isparta");
@@ -9,33 +10,56 @@ let config = require("../config");
 let src = config.globs.src;
 let out = config.globs.out;
 
-let instrument = (sourceGlob, reportDir, doExit, callback) => {
+let instrumentSource = (glob) => {
     return new Promise((resolve, reject) => {
-        gulp.src(sourceGlob)
+        return gulp.src(glob)
             .pipe(istanbul({
                 instrumenter: isparta.Instrumenter,
                 includeUntested: true
             }))
             .pipe(istanbul.hookRequire())
-            .on("finish", () => {
+            .on("finish", resolve)
+            .on("error", reject)
+    });
+};
+
+let writeReports = (reportDir) => {
+    return istanbul.writeReports({
+        dir: reportDir,
+        reporters: ["lcov", "text-summary"]
+    })
+};
+
+let instrument = (sourceGlob, reportDir, doExit, callback, tdd=false) => {
+    let handleErr = (resolve, reject) => (err) => {
+        if (tdd) {
+            gutil.log("TDD Error: ", err);
+            resolve();
+        } else {
+            reject(err);
+        }
+    };
+    return instrumentSource(sourceGlob)
+        .then(() => {
+            return new Promise((resolve, reject) => {
                 callback()
-                    .pipe(istanbul.writeReports({
-                        dir: reportDir,
-                        reporters: ["lcov", "text-summary"]
-                    }))
+                    .on("error", handleErr(resolve, reject))
+                    .pipe(writeReports(reportDir))
                     .pipe(doExit ? exit() : empty())
                     .on("end", resolve)
-                    .on("error", reject);
+                    .on("error", handleErr(resolve, reject));
             });
-    });
+        });
 };
 
 let runTestBatch = (testGlob) => {
     return gulp.src(testGlob).pipe(mocha({reporter: process.env.TEST_REPORTER || "spec"}));
 };
 
-let runTests = (testGlob, sourceGlob, reportDir, doExit=true) => () => instrument(sourceGlob, reportDir, doExit, () => runTestBatch(testGlob));
+let runTests = (testGlob, sourceGlob, reportDir, doExit = true, tdd=false) => () => instrument(sourceGlob, reportDir, doExit, () => runTestBatch(testGlob), tdd);
 
 gulp.task("server-unit-test", runTests(src.SERVER_TESTS, src.SERVER_JS, out.SERVER_COVERAGE_OUTPUT));
+gulp.task("server-unit-test-tdd", runTests(src.SERVER_TESTS, src.SERVER_JS, out.SERVER_COVERAGE_OUTPUT, true, true));
 gulp.task("client-unit-test", runTests(src.CLIENT_TESTS, src.CLIENT_JS, out.CLIENT_COVERAGE_OUTPUT, false));
+gulp.task("client-unit-test-tdd", runTests(src.CLIENT_TESTS, src.CLIENT_JS, out.CLIENT_COVERAGE_OUTPUT, false, true));
 gulp.task("test", runTests(src.ALL_TESTS, src.COVERAGE_JS, out.COMBINED_COVERAGE_OUTPUT));
