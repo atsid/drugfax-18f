d99aeceb77f748538216af3ebfc42abd20cb8117
Chris Trevino <chris.trevino@atsid.com>
2015-06-25 20:34:43 -0700

    #99 - Implementing My Drugs view

--- client/components/myDrugs.js
+++ client/components/myDrugs.js
@@ -1,10 +1,60 @@
 "use strict";
-
 let React = require("react/addons");
+let Loader = require("./common/loader");
+let ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+let Bluebird = require("bluebird");
+let DrugList = require("./drugList");
+
+let SubscriptionStore = require("../stores/subscriptionStore");
+let DrugStore = require("../stores/drugStore");
+
+let subscriptionStore = new SubscriptionStore();
+let drugStore = new DrugStore();
 
 let MyDrugs = React.createClass({
-    render: function() {
-        return <h1> My Drugs </h1>;
+    getInitialState: function () {
+        return {
+            loading: true,
+            subscriptions: []
+        };
+    },
+
+    componentDidMount: function () {
+        this.getStateFromStore();
+    },
+
+    getStateFromStore: function () {
+        this.setState({loading: true});
+        subscriptionStore.list({
+            fields: "openfda.brand_name"
+        })
+            .then((result) => result.items)
+            .then((subscriptions) => Bluebird.all(subscriptions.map((sub) => drugStore.get(sub.splSetId))))
+            .then((subscribedDrugs) => {
+                this.setState({subscriptions: subscribedDrugs, loading: false});
+            });
+    },
+
+    render: function () {
+        let subs = [];
+        this.state.subscriptions.forEach((sub) => {
+            subs.push(<li><span>{sub.openfda.brand_name[0]}</span></li>);
+        });
+
+        return (
+            <div>
+                <ReactCSSTransitionGroup component="div" transitionName="transition" transitionAppear={true}>
+                    { !this.state.loading ?
+                        <div>
+                            <h1>Saved Drugs</h1>
+                            <DrugList key={this.state.value} data={this.state.subscriptions} />
+                        </div>
+                        : null
+                    }
+                    { this.state.loading ? <Loader/> : null }
+                </ReactCSSTransitionGroup>
+            </div>
+        );
     }
 });
 
