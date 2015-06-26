3056e50f8289c2e9570fe535baba431a3de18a14
David Tittsworth <stopyoukid@gmail.com>
2015-06-26 14:38:14 -0700

    #78 - Fixed lint issue

--- server/routers/manufacturers/routers/by-name.js
+++ server/routers/manufacturers/routers/by-name.js
@@ -1,6 +1,5 @@
 "use strict";
 let jefferson = require("express-jefferson");
-let debug = require("../../../middleware/debug");
 let manufacturers = require("../../../middleware/manufacturers");
 
 module.exports = jefferson.router({
