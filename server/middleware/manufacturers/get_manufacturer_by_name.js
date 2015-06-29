793c95a243eb58081f7721fb0de7cb690c78018f
David Tittsworth <stopyoukid@gmail.com>
2015-06-29 06:58:21 -0700

    #121 - Switch from a path param to a query arg

--- server/middleware/manufacturers/get_manufacturer_by_name.js
+++ server/middleware/manufacturers/get_manufacturer_by_name.js
@@ -6,7 +6,7 @@ let manufacturerStats = require("./manufacturer_stats");
  * Gets a specific manufacturer by name
  */
 module.exports = (req, res) => {
-    let name = req.params.name;
+    let name = req.query.name;
     manufacturerStats(name)
         .then((data) => {
             res.json({
