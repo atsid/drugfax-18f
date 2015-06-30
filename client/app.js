940f9de43b21c9cbbb78f2d42b397351bb8cdeef
Chris Trevino <chris.trevino@atsid.com>
2015-06-30 12:05:20 -0700

    #161 - Renaming 'routes' to 'drugax'

--- client/app.js
+++ client/app.js
@@ -1,10 +1,10 @@
 "use strict";
 let React = require("react/addons");
-let Routes = require("./components/routes");
+let Drugfax = require("./components/drugfax");
 window.onload = function () {
     // Facebook Authentication adds this value to the location hash
     if (window.location.hash.indexOf("_=_") > -1) {
         window.location.hash = "";
     }
-    React.render(<Routes />, document.getElementById("app"));
+    React.render(<Drugfax />, document.getElementById("app"));
 };
