cd5e6a618970f227cd6d00e97466ed8d71141e50
Chris Trevino <chris.trevino@atsid.com>
2015-07-01 11:00:15 -0700

    #82 - Renaming the 'Routes' component to 'Drugfax' in the test

--- client/components/drugfax.spec.js
+++ client/components/drugfax.spec.js
@@ -3,12 +3,12 @@ require("../common.spec/spec.helpers");
 let { expect } = require("chai");
 let React = require("react/addons");
 let ReactTestUtils = React.addons.TestUtils;
-let Routes = require("./drugfax");
+let Drugfax = require("./drugfax");
 
 describe("Routes Component", () => {
     it("should load", () => {
         let renderedComponent = ReactTestUtils.renderIntoDocument(
-            <Routes />
+            <Drugfax />
         );
         expect(renderedComponent).to.exist;
     });
